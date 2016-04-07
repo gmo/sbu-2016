3aede049c15c9d4bba5c0fac83e0a4c77834f018
Justin Schaeffer <justinschaeffer@yahoo.com>
2016-04-07 21:34:28 +0000

    able to add symbols and numbers into new keywords.

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,5 +1,4 @@
 class Keyword < ActiveRecord::Base
     before_save { |keywords| keywords.word.downcase! }
-    validates :word, :on => :create, :format => { with: /\A[a-z]+\Z/, :message => 'can only contain letters'}
     validates :word, :on => :create, :uniqueness => {:presence => true, :message => 'already exists'} 
 end
