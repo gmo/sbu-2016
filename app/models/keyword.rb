2658836c567c918314e9739ce5bbb6991f11d843
Justin Schaeffer <justinschaeffer@yahoo.com>
2016-04-14 03:05:51 +0000

    formatted buttons, text, text boxes

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,3 @@
 class Keyword < ActiveRecord::Base
     before_save { |keywords| keywords.word.downcase! }
-    validates :word, :on => :create, :uniqueness => {:presence => true, :message => 'already exists'} 
 end
